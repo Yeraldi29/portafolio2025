4a99265b59a06899bcab8fbb4ad827ffba8c9f8a
Yeraldi29 <yeraldimujicarosales@gmail.com>
2025-02-20 01:15:44 -0400

    Addes skills section

---
+++ src/lib/components/Skills.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+  import JavascriptIcon from './icons/JavascriptIcon.svelte';
+  import LaravelIcon from './icons/LaravelIcon.svelte';
+  import NextjsIcon from './icons/NextjsIcon.svelte';
+  import NodejsIcon from './icons/NodejsIcon.svelte';
+  import PhpIcon from './icons/PhpIcon.svelte';
+  import ReactIcon from './icons/ReactIcon.svelte';
+  import SqlIcon from './icons/SqlIcon.svelte';
+  import SupabaseIcon from './icons/SupabaseIcon.svelte';
+  import SvelteIcon from './icons/SvelteIcon.svelte';
+  import SveltekitIcon from './icons/SveltekitIcon.svelte';
+  import TailwindcssIcon from './icons/TailwindcssIcon.svelte';
+  import TypescriptIcon from './icons/TypescriptIcon.svelte';
+</script>
+
+<section class="mt-8">
+  <h2 class="font-bold text-2xl">Skills</h2>
+
+  <div class="flex flex-wrap items-center space-x-3 gap-3 text-sm mt-4">
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <JavascriptIcon />
+      <span>Javascript</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <TypescriptIcon />
+      <span>Typescript</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <PhpIcon />
+      <span>Php</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <NodejsIcon />
+      <span>Node.js</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <SvelteIcon />
+      <span>Svelte</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <ReactIcon />
+      <span>React</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <SveltekitIcon />
+      <span>Sveltekit</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <NextjsIcon />
+      <span>Next.js</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <TailwindcssIcon />
+      <span>Tailwindcss</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <LaravelIcon />
+      <span>Laravel</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <SqlIcon />
+      <span>Sql</span>
+    </div>
+    <div
+      class="flex items-center space-x-2 px-3 py-2 border-2 border-antiflashWhite rounded-3xl"
+    >
+      <SupabaseIcon />
+      <span>Supabase</span>
+    </div>
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Header from "$lib/components/Header.svelte";
+  import Skills from "$lib/components/Skills.svelte";
 
 </script>
 
 <Header />
+<Skills />
